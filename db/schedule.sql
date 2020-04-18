daa6470f5337adc7fcc9c0b91f36dcae1ad560b0
Alexey <alexey.chukhryaev@gmail.com>
2020-04-18 23:29:54 +0300

    DB scheme update

--- db/schedule.sql
+++ db/schedule.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS `billdb`.`Schedule` (
-  `SKEY` INT NOT NULL AUTO_INCREMENT,
-  `GUID` VARCHAR(32) NULL,
-  `body` VARCHAR(65536) NULL,
+  `SKEY` INT(11) NOT NULL AUTO_INCREMENT,
+  `GUID` VARCHAR(32) NULL DEFAULT NULL,
+  `body` MEDIUMTEXT NULL DEFAULT NULL,
   `date` DATE NOT NULL,
-  `Monitors_SKEY` INT NOT NULL,
+  `Monitors_SKEY` INT(11) NOT NULL,
   PRIMARY KEY (`SKEY`, `Monitors_SKEY`),
   INDEX `fk_Schedule_Monitors1_idx` (`Monitors_SKEY` ASC) VISIBLE,
   CONSTRAINT `fk_Schedule_Monitors1`
     FOREIGN KEY (`Monitors_SKEY`)
-    REFERENCES `andrew_test`.`Monitors` (`SKEY`)
+    REFERENCES `billdb`.`Monitors` (`SKEY`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
--- db/users.sql
+++ db/users.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS `billdb`.`users` (
-  `SKEY` INT NOT NULL AUTO_INCREMENT,
+  `SKEY` INT(11) NOT NULL AUTO_INCREMENT,
   `GUID` CHAR(32) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `name` VARCHAR(120) NULL,
-  `password` CHAR(256) NOT NULL,
-  `status` TINYINT NOT NULL DEFAULT -100 COMMENT '-100 Banned\n-50 Unactive\n0 Active\n',
-  `role` TINYINT NOT NULL DEFAULT 0 COMMENT '0 No permission\n1 User\n5 Moderator\n10 Admin',
+  `name` VARCHAR(120) NULL DEFAULT NULL,
+  `password` CHAR(255) NOT NULL,
+  `status` TINYINT(4) NOT NULL DEFAULT '-100' COMMENT '-100 Banned\\n-50 Unactive\\n0 Active\\n',
+  `role` TINYINT(4) NOT NULL DEFAULT '0' COMMENT '0 No permission\\n1 User\\n5 Moderator\\n10 Admin',
   `created` DATETIME NOT NULL,
   PRIMARY KEY (`SKEY`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
 ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
