9dd3d8e8f8d3dedfc625bf69141c8b408c2b7d82
Alexey <alexey.chukhryaev@gmail.com>
2020-05-01 15:39:41 +0300

    [webdata] Monitors data methods completed and tested with stupid tests

--- webdata/monitors_data.py
+++ webdata/monitors_data.py
@@ -5,11 +5,38 @@ import datetime
 
 def list_monitors():
     try:
-        sql = "SELECT * from Monitors"
+        sql = "SELECT GUID, name, location from Monitors"
         monitors = database_instance.fetchall(sql)
         return dict(
-            {"code": "get.list",
-             "monitors": [{"guid": monitor[1], "name": monitor[2], "location": monitor[3]} for monitor in monitors]})
+            {"code": "get.monitors.list", "monitors": [{
+                "guid": monitor[0],
+                "name": monitor[1],
+                "location": monitor[2]
+            } for monitor in monitors]})
+    except Exception as e:
+        return dict({"code": "error", "error": e})
+
+
+def update_monitor(guid, json_body):
+    try:
+        sql = "UPDATE Monitors SET name = %s, location = %s WHERE GUID = %s"
+        val = (json_body.get("name"), json_body.get("location"), guid)
+        result = database_instance.execute(sql, val)
+        return result
+    except Exception as e:
+        return dict({"code": "error", "error": e})
+
+
+def get_monitor(guid):
+    try:
+        sql = "SELECT GUID, name, location FROM Monitors WHERE GUID = %s"
+        val = [guid]
+        result = database_instance.fetchone(sql, val)
+        return dict({"code": "get.monitor", "monitor": {
+            "guid": result[0],
+            "name": result[1],
+            "location": result[2]
+        }})
     except Exception as e:
         return dict({"code": "error", "error": e})
 
@@ -26,10 +53,17 @@ def create_monitor(json_body):
         sql = "INSERT INTO Monitors (SKEY, GUID, name, location) VALUES (%s, %s, %s, %s)"
         val = (None, guid, name, location)
         result = database_instance.execute(sql, val)
-        if result.rowcount == 1:
-            return list_monitors()
-    except IndexError as e:
+        return result
+    except Exception as e:
         return dict({"code": "error", "error": e})
+
+
+def delete_monitor(guid):
+    try:
+        sql = "DELETE FROM Monitors WHERE GUID = %s"
+        val = [guid]
+        result = database_instance.execute(sql, val)
+        return result
     except Exception as e:
         return dict({"code": "error", "error": e})
 
@@ -41,5 +75,15 @@ if __name__ == "__main__":
             "location": f"EUROPA{str(datetime.datetime.today())}"
         }
     }
+    guid = "e58eea456388e3abf4429d3ad06094c4"
+    update_body = {
+        "name": "NOT TEST DEV MONITOR",
+        "location": f"EUROPA{str(datetime.datetime.today())}"
+    }
     print(create_monitor(body))
+    print(get_monitor(guid))
+    print(update_monitor(guid, update_body))
+    print(get_monitor(guid))
+    print(delete_monitor(guid))
+    print(get_monitor(guid))
     print(list_monitors())
