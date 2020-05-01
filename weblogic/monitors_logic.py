0f7b773cbe4ba83e1bcaba3c2a7fb4957ffdfc77
Alexey <alexey.chukhryaev@gmail.com>
2020-05-01 15:40:21 +0300

    [weblogic] Monitors logic completed (NEED TEST)

--- weblogic/monitors_logic.py
+++ weblogic/monitors_logic.py
@@ -2,22 +2,55 @@ from webdata import monitors_data
 
 
 def get_monitors():
-	payload = monitors_data.list_monitors()
-	return payload
+	try:
+		payload = monitors_data.list_monitors()
+		return payload
+	except Exception as e:
+		return dict({"code": "error", "error": e})
 
 
-def post_monitor(guid):
-	pass
+def post_monitor(guid, json_body):
+	try:
+		payload = monitors_data.update_monitor(guid, json_body)
+		if payload.rowcount == 1:
+			payload = get_monitors()
+			return payload
+		return dict({"code": "error", "error": f"Something happened while updating monitor {guid}"})
+	except IndexError as e:
+		return dict({"code": "error", "error": e})
+	except Exception as e:
+		return dict({"code": "error", "error": e})
 
 
 def get_monitor(guid):
-	pass
+	try:
+		payload = monitors_data.get_monitor(guid)
+		return payload
+	except Exception as e:
+		return dict({"code": "error", "error": e})
 
 
 def post_monitor_create(json_body):
-	payload = monitors_data.create_monitor(json_body)
-	return payload
+	try:
+		payload = monitors_data.create_monitor(json_body)
+		if payload.rowcount == 1:
+			payload = get_monitors()
+			return payload
+		return dict({"code": "error", "error": "Something happened while creating new monitor"})
+	except IndexError as e:
+		return dict({"code": "error", "error": e})
+	except Exception as e:
+		return dict({"code": "error", "error": e})
 
 
 def delete_monitor(guid):
-	pass
+	try:
+		payload = monitors_data.delete_monitor(guid)
+		if payload.rowcount == 1:
+			payload = get_monitors()
+			return payload
+		return dict({"code": "error", "error": f"Something happened while deleting monitor {guid}"})
+	except IndexError as e:
+		return dict({"code": "error", "error": e})
+	except Exception as e:
+		return dict({"code": "error", "error": e})
