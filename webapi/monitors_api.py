97d150847241939765c2570232223300eca21644
Alexey <alexey.chukhryaev@gmail.com>
2020-05-15 02:18:09 +0300

    UI call

--- webapi/monitors_api.py
+++ webapi/monitors_api.py
@@ -1,17 +1,21 @@
 from flask import request
+from flask import render_template
 from weblogic import monitors_logic
 from webapi import *
 
 
 @app.route('/monitors', methods=['GET'])
 def get_monitors():
-	return monitors_logic.get_monitors()
+	data = monitors_logic.get_monitors()
+	# return data
+	return render_template('monitors.html', data=data)
 
 
 @app.route('/monitor/<guid>', methods=['GET', 'POST', 'DELETE'])
 def guid_monitor(guid):
 	if request.method == "GET" and guid:
-		return monitors_logic.get_monitor(guid)
+		data = monitors_logic.get_monitor(guid)
+		return render_template('monitor.html', data=data)
 	if request.method == "POST" and guid:
 		return monitors_logic.post_monitor(guid, request.json)
 	if request.method == "DELETE" and guid:
